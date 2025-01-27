c17687aab86dec59840c5ca6cb293040d43ee5e3
ananyayayaya <ananya.gla_cs22@gla.ac.in>
2025-01-27 09:04:01 +0530

    changes by ananya

--- server/index.js
+++ server/index.js
@@ -3,20 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./MongoDb/db');
 const todoRoutes = require('./routes/todoRoutes');
+const authRoutes = require('./routes/authRoutes'); 
 const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
+
 
 app.use(express.json());
 app.use(cors());
 
-connectDB(MONGO_URI);//sakshambkl
 
+connectDB(MONGO_URI);
 
-app.use('/api/todos', todoRoutes);
+// Routes
+app.use('/api/todos', todoRoutes); 
+app.use('/api/auth', authRoutes);  
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
